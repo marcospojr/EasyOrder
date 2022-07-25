41100f6866fa9793842fcffcff4a8136226228a6
Marcos Jr <marcospojr13@gmail.com>
2022-07-24 21:10:47 -0300

    Creating AppBar

--- easy_order/lib/main.dart
+++ easy_order/lib/main.dart
@@ -21,11 +21,62 @@ class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("My first app"),
-          ),
-          body: Column()),
+      debugShowCheckedModeBanner: false,
+      home: scaffold,
+    );
+  }
+
+// Widgets
+
+// Scaffold Widget
+  var scaffold = Scaffold(
+      appBar: AppBar(
+        title: const Text("Bar do Tião"),
+        backgroundColor: Colors.red,
+        leading: const OptionsButton(),
+        actions: const <Widget>[OrderButton()],
+      ),
+      body: Column());
+}
+
+// OrderButton Widget
+class OrderButton extends StatelessWidget {
+  const OrderButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(
+        Icons.grading_sharp,
+        color: Colors.white,
+      ),
+      onPressed: () {
+        // TODO: - implementar a ação do botão
+        // do something
+      },
+    );
+  }
+}
+
+// OptionsButton Widget
+class OptionsButton extends StatelessWidget {
+  const OptionsButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: const Icon(
+        Icons.flatware,
+        color: Colors.white,
+      ),
+      onPressed: () {
+        // TODO: - implementar a ação do botão
+        // do something
+      },
     );
   }
 }
